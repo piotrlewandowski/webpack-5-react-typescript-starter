5ea43918e3d07781f49cac92f1937ef9c6461412
Piotr Lewandowski <contact@piotrlewandowski.info>
2022-07-24 00:15:26 +0100

    feat: add StrictMode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,9 @@ import WebpackLogo from '../images/webpack-logo.svg';
 
 export const App: React.FC = (): ReactElement => {
   return (
-    <>
+    <React.StrictMode>
       <h1>Webpack 5 React TypeScript Starter</h1>
       <WebpackLogo />
-    </>
+    </React.StrictMode>
   );
 };
